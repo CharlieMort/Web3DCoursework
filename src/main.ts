5d60b6d1d410333c479eaf09935c005bd1456548
CharlieMort <charlie_mort@outlook.com>
2026-04-24 11:49:38 +0100

    updated the main page and added model highlights

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,79 @@ import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min';
 import { DRACOLoader, GLTF, GLTFLoader } from 'three/examples/jsm/Addons';
 import Model from './model';
 
+function ModelViewer(id: string, cameraPos: THREE.Vector3, name: string, path: string, target: THREE.Vector3 = new THREE.Vector3(0, 5, 0)) {
+	let canvas = document.querySelector(id)
+	if (canvas == null) return;
+
+	const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+	renderer.shadowMap.enabled = true
+	const fov = 45;
+	const aspect = 2; // the canvas default
+	const near = 0.1;
+	const far = 100;
+	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+	camera.position.add(cameraPos)
+
+	const controls = new OrbitControls( camera, canvas );
+	controls.target.add(target)
+	controls.update();
+
+	const scene = new THREE.Scene();
+	scene.background = new THREE.Color( '#09bfff' );
+
+	let model = new Model(name, path)
+	// canvas.parentElement.append(model.playButton)
+	// canvas.parentElement.append(model.wireButton)
+
+	scene.add(model.group)
+	{
+		const color = 0xFFFFFF;
+		const intensity = 5;
+		const light = new THREE.AmbientLight( color, intensity );
+		scene.add( light );
+	}
+
+	{
+		const color = 0xFFFFFF;
+		const intensity = 5;
+		const light = new THREE.DirectionalLight(color, intensity);
+		light.position.set(50,50,50)
+		light.lookAt(new THREE.Vector3(0,0,0))
+		light.castShadow = true
+		scene.add (light)
+	}
+
+	function resizeRendererToDisplaySize( renderer: THREE.WebGLRenderer ) {
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if ( needResize ) {
+
+			renderer.setSize( width, height, false );
+
+		}
+		return needResize;
+	}
+
+	function render() {
+		requestAnimationFrame( render );
+		if ( resizeRendererToDisplaySize( renderer ) ) {
+			const canvas = renderer.domElement;
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
+		}
+
+		model.update()
+		model.group.rotation.y += 0.01;
+
+		renderer.render( scene, camera );
+	}
+
+	requestAnimationFrame( render );
+}
 
 function main() {
-
 	const canvas: HTMLCanvasElement | null = document.querySelector( '#c' );
     if (canvas == null) return;
 	const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
@@ -50,8 +120,10 @@ function main() {
 
 	}
 	
-	let slotMachine = new Model("Slot Machine", "src/slotmachine2.glb")
+	let slotMachine = new Model("Slot Machine", "src/slotmachine3.glb")
 	let clawMachine = new Model("Claw Machine", "src/clawMachine.glb")
+	let robot = new Model("Robot", "src/robot.glb")
+	robot.group.scale.set(0.2, 0.2, 0.2)
 
 	slotMachine.setPosition(10, 0, -10)
 	slotMachine.setRotation(0, -90, 0)
@@ -61,39 +133,21 @@ function main() {
 
 	scene.add(slotMachine.group)
 	scene.add(clawMachine.group)
+	scene.add(robot.group)
 
-
-	class ColorGUIHelper {
-        object: any;
-        prop: any;
-
-		constructor( object: any, prop: any ) {
-			this.object = object;
-			this.prop = prop;
-		}
-		get value() {
-
-			return `#${this.object[ this.prop ].getHexString()}`;
-
-		}
-		set value( hexString ) {
-
-			this.object[ this.prop ].set( hexString );
-
-		}
+	{
+		const light = new THREE.PointLight(0xffffff, 100, 100);
+		light.position.set(clawMachine.group.position.x, clawMachine.group.position.y+12, clawMachine.group.position.z)
+		
+		scene.add(light)
 
 	}
 
 	{
-
 		const color = 0xFFFFFF;
-		const intensity = 3;
+		const intensity = 4;
 		const light = new THREE.AmbientLight( color, intensity );
 		scene.add( light );
-
-		const gui = new GUI();
-		gui.addColor( new ColorGUIHelper( light, 'color' ), 'value' ).name( 'color' );
-		gui.add( light, 'intensity', 0, 5, 0.01 );
 
 	}
 
@@ -137,3 +191,6 @@ function main() {
 }
 
 main();
+ModelViewer("#slotmachine", new THREE.Vector3(0, 20, 25), "Slot Machine", "src/slotmachine3.glb")
+ModelViewer("#robot", new THREE.Vector3(0, 30, 40), "Robot", "src/robot.glb", new THREE.Vector3(0, 10, 0))
+ModelViewer("#clawmachine", new THREE.Vector3(0, 20, 25), "Claw Machine", "src/clawmachine.glb")
--- src/model.ts
+++ src/model.ts
@@ -7,22 +7,42 @@ class Model {
     mixer: THREE.AnimationMixer
     actions: THREE.AnimationAction[];
     model: GLTF
+    modelScene: GLTF["scene"]
     name: string
     group: THREE.Group<THREE.Object3DEventMap>;
+    path: string;
+    wireframe: boolean;
+    playButton: HTMLButtonElement;
+    wireButton: HTMLButtonElement;
 
     constructor(name: string, path: string) {
         this.clock = new THREE.Clock()
         this.name = name;
         this.group = new THREE.Group()
-        this.loadModel(path)
-        
+        this.path = path;
+        this.wireframe = false;
+        this.loadModel()
+        this.loadButtons()
     }
 
-    loadModel(path: string) {
+    toggleWireframe() {
+        this.wireframe = !this.wireframe
+
+        if (this.model) {
+            this.model.scene.traverse((child) => {
+                if (child.isMesh) {
+                    child.material.wireframe = this.wireframe;
+                }
+            })
+        }
+    }
+
+    loadModel() {
         const loader = new GLTFLoader();
         this.actions = []
-        loader.load(path, (x) => {
+        loader.load(this.path, (x) => {
             this.model = x;
+            this.modelScene = x.scene
             this.group.add(this.model.scene)
             this.mixer = new THREE.AnimationMixer(this.model.scene)
             const animations = this.model.animations;
@@ -30,20 +50,26 @@ class Model {
                 const action = this.mixer.clipAction(clip)
                 this.actions.push(action)
             })
+        })
+    }
 
-            let playButton = document.createElement("button")
-            playButton.innerText = "Play " + this.name
-            playButton.addEventListener("click", () => {
-                console.log("playing animation for " + this.name)
-                this.actions.forEach(action => {
-                    action.timeScale = 1;
-                    action.setLoop(THREE.LoopOnce, 1)
-                    action.reset()
-                    action.play()
-                }) 
-            })
+    loadButtons() {
+        this.playButton = document.createElement("button")
+        this.playButton.innerText = "Play Animation"
+        this.playButton.addEventListener("click", () => {
+            console.log("playing animation for " + this.name)
+            this.actions.forEach(action => {
+                action.timeScale = 1;
+                action.setLoop(THREE.LoopOnce, 1)
+                action.reset()
+                action.play()
+            }) 
+        })
 
-            document.body.append(playButton)
+        this.wireButton = document.createElement("button")
+        this.wireButton.innerText = "Toggle WireFrame"
+        this.wireButton.addEventListener("click", () => {
+            this.toggleWireframe()
         })
     }
 
